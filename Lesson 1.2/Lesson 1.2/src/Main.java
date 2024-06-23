7bc389e7731e449b113abc7464bcf9a7e9bf6637
mihan <maleksyutin@bk.ru>
2024-06-23 14:18:09 +0300

    Урок 1.2

---
+++ Lesson 1.2/Lesson 1.2/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("************************");
+        System.out.println("****    Визитка     ****");
+        System.out.println("**** Name: Михаил   ****");
+        System.out.println("**** Skype Михан 78 ****");
+        System.out.println("************************");
+    }
+}
