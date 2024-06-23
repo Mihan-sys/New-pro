6ab04235dca5c9a624f0831ff37321da2677137c
mihan <maleksyutin@bk.ru>
2024-06-23 14:20:53 +0300

    Урок 1.2 Добавил 99999

--- Lesson 1.2/Lesson 1.2/src/Main.java
+++ Lesson 1.2/Lesson 1.2/src/Main.java
@@ -3,7 +3,7 @@ public class Main {
         System.out.println("************************");
         System.out.println("****    Визитка     ****");
         System.out.println("**** Name: Михаил   ****");
-        System.out.println("**** Skype Михан 78 ****");
+        System.out.println("**** Skype Михан 99999 ****");
         System.out.println("************************");
     }
 }
